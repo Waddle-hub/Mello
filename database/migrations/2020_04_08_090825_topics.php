f713abb2feb3c245a17ff9d13c3bc25023d4fa3c
Waddle-hub <Wadefriedrichs42@gmail.com>
2020-04-08 11:13:31 +0200

    Added topics

---
+++ database/migrations/2020_04_08_090825_topics.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Topics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        schema::create('topics', , function (Blueprint $table) {
+            $table->bigInteger('topic_id')->unsigned();
+            $table->string('topic_name');
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
