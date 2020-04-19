4f2167ea89865eb486d222dde011c90b66c56802
56112755 <Wadefriedrichs42@gmail.com>
2020-04-19 22:04:24 +0200

    fresh start

--- resources/views/Posts/postitem.blade.php
+++
@@ -1,9 +0,0 @@
-<article class="card">
-    <div class="card-header">
-        {{ $post->title }}
-    </div>
-    <div class="card-body">
-        <p>{{ $post->topic->title }}</p>
-        <p>{{ $post->author->firstname }}</p>
-    </div>
-</article>
--- resources/views/home/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('Layout.Master')
-
-@section('content')
-    @foreach($posts as $post)
-       @include('posts._item')
-    @endforeach
-@endsection
