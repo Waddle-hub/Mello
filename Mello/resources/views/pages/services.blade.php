deb1aa3b2e6863494e49b372ea52458fbacada9f
56112755 <Wadefriedrichs42@gmail.com>
2020-04-19 23:36:51 +0200

    changes to files removing redudant html

--- Mello/resources/views/pages/services.blade.php
+++ Mello/resources/views/pages/services.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'Mello')}}</title>
+@section('content')
+<h1>Services</h1>
+<p>this is the services page</p>
+@endsection
 
-    </head>
-    <body>
-        <h1>Services</h1>
-        <p>this is the services page</p>
-    </body>
-</html>
+    
