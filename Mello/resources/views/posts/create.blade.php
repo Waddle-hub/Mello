ce9fd5b3462445823bfcbb66f4b0123bb9f56e86
56112755 <Wadefriedrichs42@gmail.com>
2020-04-20 11:45:05 +0200

    made a form for input

--- Mello/resources/views/posts/create.blade.php
+++ Mello/resources/views/posts/create.blade.php
@@ -2,5 +2,18 @@
 
 @section('content')
     <h1>Create Posts !</h1>
-   
+
+    <form method="post" action="{{ route('posts.store') }}">
+        <div class="form-group">
+            @csrf            
+            <label for="title">Title</label>
+            <input type="text" class="form-control" name="title" placeholder="Title"/>
+        </div>
+        <div class="form-group">
+            <label for="Body">Body</label>
+            <textarea class="form-control" name="Body" cols="30" rows="10" placeholder="Body Text"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Create Post</button>
+ </form>
 @endsection
