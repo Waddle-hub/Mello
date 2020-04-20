aa7aecbad0c819a2c6da86ce25238761ac01e941
56112755 <Wadefriedrichs42@gmail.com>
2020-04-20 11:41:53 +0200

    remove collection form

--- Mello/resources/views/posts/create.blade.php
+++ Mello/resources/views/posts/create.blade.php
@@ -2,10 +2,5 @@
 
 @section('content')
     <h1>Create Posts !</h1>
-    {!! Form::open(['action' => 'PostController@store', 'method'=> 'POST']) !!}
-        <div class="form-group">
-            {{Form::label('title', 'Title')}}
-            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
-        </div>
-    {!! Form::close() !!}
+   
 @endsection
